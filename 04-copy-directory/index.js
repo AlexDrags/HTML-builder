052227c06a11e01a148052a9624bb1742024fc52
AlexDrags <ksander-88@yandex.ru>
2025-01-20 02:15:46 +0300

    feat:copy-directory  complete

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,19 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+async function copyDir() {
+    try {
+        fs.mkdir(path.join('./04-copy-directory', '/files-copy'), { recursive: true }, () => {});
+    
+        const files = await fs.readdir(`${path.join('./04-copy-directory/files')}`, () => {});
+        for (const element of files) {
+            console.log(path.join('./04-copy-directory/files-copy'));
+            fs.copyFile(`${path.join('./04-copy-directory/files', `/${element}`)}`, `${path.join('./04-copy-directory/files-copy', `/${element}`)}`, 0, () => {});
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+    
+}
+
+copyDir();
